f1cf86429a8197fdbf8b89eba376e3ae3e17d6de
prashantkumar521520 <prashant.kumar2@thoughtworks.com>
2023-02-03 14:11:59 +0530

    add logging

--- src/employeeservice/src/main/java/com/example/employeeservice/controller/EmployeeController.java
+++ src/employeeservice/src/main/java/com/example/employeeservice/controller/EmployeeController.java
@@ -1,7 +1,7 @@
 package com.example.employeeservice.controller;
 
 import java.util.List;
-
+import static net.logstash.logback.argument.StructuredArguments.keyValue;
 import com.example.employeeservice.model.Employee;
 import com.example.employeeservice.repository.EmployeeRepository;
 import org.slf4j.Logger;
@@ -23,31 +23,31 @@ public class EmployeeController {
 
     @PostMapping("/")
     public Employee add(@RequestBody Employee employee) {
-        LOGGER.info("Employee add: {}", employee);
+        LOGGER.info("Employee add: {}", keyValue("employee", employee));
         return repository.save(employee);
     }
 
     @GetMapping("/{id}")
     public Employee findById(@PathVariable("id") String id) {
-        LOGGER.info("Employee find: id={}", id);
+        LOGGER.info("Employee find: id={}", keyValue("id", id));
         return repository.findById(id).get();
     }
 
     @GetMapping("/")
     public Iterable<Employee> findAll() {
-        LOGGER.info("Employee find");
+        LOGGER.info("Employee find",keyValue("path", "/"));
         return repository.findAll();
     }
 
     @GetMapping("/department/{departmentId}")
     public List<Employee> findByDepartment(@PathVariable("departmentId") String departmentId) {
-        LOGGER.info("Employee find: departmentId={}", departmentId);
+        LOGGER.info("Employee find: departmentId={}", keyValue("path", "/department/{departmentId}"));
         return repository.findByDepartmentId(departmentId);
     }
 
     @GetMapping("/organization/{organizationId}")
     public List<Employee> findByOrganization(@PathVariable("organizationId") String organizationId) {
-        LOGGER.info("Employee find: organizationId={}", organizationId);
+        LOGGER.info("Employee find: organizationId={}", keyValue("path", "/organization/{organizationId}"));
         return repository.findByOrganizationId(organizationId);
     }
 
